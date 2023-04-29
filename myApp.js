799dba6c359a8c17862d2abedaa319374a904432
João Linhares Gomes <linharesgomes.joao@gmail.com>
2023-04-30 00:07:43 +0100

    Challenge 4: add bind static middleware to the /public path

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+app.use('/public', express.static(`${__dirname}/public`));
 
 app.get('/', (_, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
