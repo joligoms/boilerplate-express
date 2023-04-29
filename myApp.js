3deb2fd80d73507971526695f81137603bdcf14c
João Linhares Gomes <linharesgomes.joao@gmail.com>
2023-04-30 00:02:55 +0100

    Challenge 3: send index.html file in the root route

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (_, res) => res.send('Hello Express'));
+app.get('/', (_, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+});
 
  module.exports = app;
